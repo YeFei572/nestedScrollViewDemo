a023ffb82658715baa5fc5e5246e60ae3c259317
YeFei <YeFei@v2ss.cn>
2022-11-20 15:57:08 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
   if (Platform.isAndroid) {
     /// 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
@@ -58,6 +60,8 @@ class _MyHomePageState extends State<MyHomePage>
         setState(() => shown = false);
       }
     });
+    WidgetsBinding.instance
+        .addPostFrameCallback((timeStamp) async => buildAlertDialog(context));
   }
 
   @override
@@ -83,58 +87,91 @@ class _MyHomePageState extends State<MyHomePage>
         body: TabBarView(
           controller: _tabController,
           children: [
-            _buildContent(),
-            _buildContent(),
+            _buildContent(context),
+            _buildContent(context),
           ],
         ),
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return <Widget>[
-            SliverAppBar(
-              pinned: true,
-              expandedHeight: 200,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Image.asset(
-                  'assets/images/img.png',
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-              floating: true,
-              bottom: TabBar(
-                controller: _tabController,
-                tabs: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    child: const Text('节假日'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    child: const Text('工作日'),
-                  ),
-                ],
-              ),
-            )
+            buildSliverAppBar(),
           ];
         },
       ),
     );
   }
 
-  Widget _buildContent() {
+  SliverAppBar buildSliverAppBar() {
+    return SliverAppBar(
+      pinned: true,
+      expandedHeight: 200,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Image.asset(
+          'assets/images/img.png',
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+      floating: true,
+      bottom: TabBar(
+        controller: _tabController,
+        tabs: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: const Text('节假日'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: const Text('工作日'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: List.generate(
           100,
-          (index) => Container(
-            decoration: BoxDecoration(
-              color: Colors.primaries[index % Colors.primaries.length],
-              borderRadius: BorderRadius.circular(20),
+          (index) => InkWell(
+            onTap: () => buildAlertDialog(context),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.primaries[index % Colors.primaries.length],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              margin: const EdgeInsets.all(20),
+              height: 100,
+              width: double.infinity,
             ),
-            margin: const EdgeInsets.all(20),
-            height: 100,
-            width: double.infinity,
           ),
         ),
       ),
     );
+  }
+
+  buildAlertDialog(BuildContext context) async {
+    final result = await showOkCancelAlertDialog(
+      context: context,
+      title: '更新提示',
+      message:
+          '1、万物更新，旧疾当愈，往事清零，爱恨随意，常安，长安。\n2、媳妇更新了她的签名：我愿用身上的10斤肉换取母亲的1年的寿命。然后岳母大人在后面评论：闺女，你妈我还不想变成千年老妖……\n3、人若是更新不了自己，只能在“旧”和“重复”之中迷惑。',
+      okLabel: '确认更新',
+      cancelLabel: '稍后再说',
+      barrierDismissible: false,
+      useActionSheetForIOS: true,
+    );
+    if (result == OkCancelResult.ok && mounted) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('同意更新')));
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('稍后更新')));
+    }
+
+    print('-----$result');
+  }
+
+  _onWillPop() {
+    return 1 == 2;
   }
 }
